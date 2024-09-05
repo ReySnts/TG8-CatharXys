27be65ac13abdef517c73e55eac3db3fa2cf620a
Axeoq <anthonysuryana2580@gmail.com>
2024-09-06 01:23:51 +0700

    Weeper Temp Commit

---
+++ BGDC-TG8/Assets/Master/Class/Enemy/Weeper/BEBulletDamage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BEBulletDamage : MonoBehaviour
+{
+    public GameEvent onPlayerDamaged;
+
+    float damageAmount = 40;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //PlayerHealth.takeDamage(damageAmount);
+            Debug.Log(damageAmount);
+            onPlayerDamaged.Raise(this, damageAmount);
+        }
+    }
+
+}
